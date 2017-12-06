6bcb2f8a94bbe09df6a2a5bc3a4c0e2fb7c0af4d
tahirkalim <thrkalim@gmail.com>
2017-12-06 04:31:17 +0100

    Associations between user and files

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -10,11 +10,11 @@ class DocsController < ApplicationController
 	end
 
 	def new
-		@doc = Doc.new
+		@doc = current_user.docs.build
 	end
 
 	def create
-		@doc = Doc.new(doc_params)
+		@doc = current_user.docs.build(doc_params)
 
 		if @doc.save
 			redirect_to @doc
@@ -36,7 +36,7 @@ class DocsController < ApplicationController
 
 	def destroy	 
 		@doc.destroy
-		redirect_to doc_path
+		redirect_to docs_path
 	end
 
 private
